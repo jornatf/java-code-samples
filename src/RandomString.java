078ac2ee671f73ba32dab069ffb9c45e4a098851
Jordan <me@jordnat.dev>
2024-01-23 11:31:06 +0100

    Create RandomString.java
    Example to generate random string.

---
+++ src/RandomString.java
@@ -0,0 +1,24 @@
+public class RandomString {
+  public static void main(String[] args) {
+    String randomStr = randomStr(120);
+    System.out.println(randomStr);
+  }
+
+  static int randomNumber(int max) {
+    double random = Math.floor(Math.random() * max);
+    return (int) random;
+  }
+
+  static String randomStr(int nbChar) {
+    String str = "";
+    String alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (int i = 0; i < nbChar; i++) {
+      int randomIndex = randomNumber(alphaNum.length());
+      char randomChar = alphaNum.charAt(randomIndex);
+      str += randomChar;
+    }
+
+    return str;
+  }
+}
